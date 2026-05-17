d9c2033728b369d8eb47728ee03f415a8f86acf8
Basil Salaita <basil.salaita8@ghmail.com>
2026-05-17 20:51:23 +0100

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const button = document.querySelector(".btn-primary");
+
+    button.addEventListener("click", function() {
+
+        button.innerHTML = "Opening...";
+
+    });
+
+});
